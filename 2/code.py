49457b4cbbcdbf073eb5b92edc4641a251b9cbd0
sandra <7448129+sandrahb4@users.noreply.github.com>
2024-12-02 22:37:18 +0100

    day 2 part 2 done

--- 2/code.py
+++ 2/code.py
@@ -4,13 +4,12 @@ def is_ordered(levels):
 	return levels == sorted(levels) or levels == sorted(levels, reverse = True)
 
 def is_safe_stepsize(levels):
-    for i, level in enumerate(levels): 
+    for i in range(len(levels)):
         if i == 0: 
         	continue
         stepsize = abs(levels[i] - levels[i - 1])
         if stepsize < 1 or stepsize > 3: 
             return False
-
     return True 
 
 safe_count = 0
@@ -29,4 +28,20 @@ safe_count = 0
 with open('input', 'r') as f:
     for report in f:
         levels = list(map(int, report.strip().split(' ')))
-        # TODO
+        safe = False 
+
+        if is_ordered(levels) and is_safe_stepsize(levels):
+            safe = True 
+
+        if not safe:
+            for i in range(len(levels)):
+                levels_mod = levels[:]
+                levels_mod.pop(i)
+                if is_ordered(levels_mod) and is_safe_stepsize(levels_mod):
+                    safe = True 
+                    break
+
+        if safe:
+            safe_count += 1
+
+print(safe_count)
