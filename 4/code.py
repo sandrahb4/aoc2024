a48b20a230137182fedf81fe9a73baffc6b8ea90
sandra <7448129+sandrahb4@users.noreply.github.com>
2024-12-04 18:29:37 +0100

    day 4 part 2

--- 4/code.py
+++ 4/code.py
@@ -1,6 +1,15 @@
 import numpy as np
 import re
 
+### Read input 
+
+data = []
+with open('input', 'r') as f:
+	for line in f:
+		data.append(list(line.strip()))
+
+matrix = np.array(data)
+
 # Part 1
 
 def count_all_possible_words_in_array(array, word): 
@@ -35,20 +44,37 @@ def count_all_diagonal_words(matrix, word):
 
 	return count
 
-data = []
-with open('input', 'r') as f:
-	for line in f:
-		data.append(list(line.strip()))
-
-matrix = np.array(data)
 word = 'XMAS'
-
 total = 0
+
 total += count_all_horizontal_words(matrix, word)
 total += count_all_horizontal_words(matrix.transpose(), word)
-total += count_all_diagonal_words(np.flip(matrix), word)
+total += count_all_diagonal_words(matrix, word)
 total += count_all_diagonal_words(np.flip(matrix, axis=0), word)
 
 print(total)
 
 ### Part 2
+
+def is_valid_xmas_diagonal(item1, item2):
+	return (item1 == 'M' and item2 == 'S') or (item1 == 'S' and item2 == 'M')
+
+def is_valid_xmas(matrix, index): 
+	if index[0] < 1 or index[0] >= matrix.shape[0]-1 or index[1] < 1 or index[1] >= matrix.shape[1]-1:
+		return False
+
+	item1_diag1 = matrix[(index[0]-1, index[1]-1)]
+	item2_diag1 = matrix[(index[0]+1, index[1]+1)]
+
+	item1_diag2 = matrix[(index[0]-1, index[1]+1)]
+	item2_diag2 = matrix[(index[0]+1, index[1]-1)]
+	
+	return is_valid_xmas_diagonal(item1_diag1, item2_diag1) and is_valid_xmas_diagonal(item1_diag2, item2_diag2)
+
+count = 0 
+for index,item in np.ndenumerate(matrix):
+	if item == 'A' and is_valid_xmas(matrix, index): 
+		count += 1
+
+print(count)
+
